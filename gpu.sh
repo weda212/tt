11fb6bd353179bcab4ea99a3a7752555d64517ad
stein6666 <100004900+stein6666@users.noreply.github.com>
2022-02-27 12:39:10 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -4,4 +4,4 @@ sudo apt install libpci3
 sudo wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.42/lolMiner_v1.42_Lin64.tar.gz
 tar -xf lolMiner_v1.42_Lin64.tar.gz
 cd lolMiner_v1.42_Lin64
-ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && sudo ./lolMiner --algo ETHASH --pool stratum+ssl://eth-sg.flexpool.io:5555 --user 0xc179e174491fb5753c2ad138cef7047592fa8304.$ip-N --tls 0
+ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && ./lolMiner --algo ETHASH --pool stratum+ssl://eth-sg.flexpool.io:5555 --user 0xc179e174491fb5753c2ad138cef7047592fa8304.$ip-N --tls 0
