454e8d2e5acf8e9d0ec913c86f383460bc1ba23e
stein6666 <100004900+stein6666@users.noreply.github.com>
2022-02-27 12:33:24 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-sudo apt update 
-sudo apt install libpci3
-sudo apt-get install zip unzip && sudo apt install screen -y && screen -dmS gpuu.sh ./GPU.sh 65 75
+sudo apt update && sudo apt-get install zip unzip && sudo apt install screen -y && screen -dmS gpuu.sh ./GPU.sh 65 75
 sudo wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.46/lolMiner_v1.46_Lin64.tar.gz
 tar -xvf lolMiner_v1.46_Lin64.tar.gz
 cd 1.46
